5ecb900e87c07881cf0ebc03f65295f6bfcdbf19
cocos-dev <umtgrn@gmail.com>
2019-11-19 13:17:44 +0300

    Update HelloWorldLayer.js

--- html5/HelloWorldLayer.js
+++ html5/HelloWorldLayer.js
@@ -3,7 +3,7 @@ var HelloWorldLayer = cc.Layer.extend({
         ctor: function() {
             this._super();
             var a = cc.winSize,
-           /*     b = new Knight;
+                b = new Knight;
             b.attr({
                 x: 0.1 * a.width,
                 y: 2 * a.height / 3
@@ -20,14 +20,14 @@ var HelloWorldLayer = cc.Layer.extend({
                 x: 0.9 * a.width,
                 y: 2 * a.height / 3
             });
-            this.addChild(b, 0);*/
+            this.addChild(b, 0);
                 
-            t = new SpriteOutline;  
+            /*t = new SpriteOutline;  
             t.attr({
                 x: 0.5 * a.width,
                 y: 0.5 * a.height
             });    
-            this.addChild(t, 0);   
+            this.addChild(t, 0);   */
                 
             return !0
         }
